aff60e7f8dd74aabeb0a8b632a2ef7758e78c3f4
ianj090 <ianjenatz@ufm.edu>
2020-10-22 12:55:36 -0600

    Added filters

--- newsworthy.py
+++ newsworthy.py
@@ -4,59 +4,76 @@ from json import loads
 from news import news
 import jinja2
 
-
-#http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-22&sortBy=publishedAt&apiKey=58e9f79801054ad698f961de67f7c258
-page = get("http://newsapi.org/v2/top-headlines?country=us&apiKey=58e9f79801054ad698f961de67f7c258") 
-page.encoding = page.apparent_encoding
-json_page = dict(loads(page.text))["articles"]
-news_list = list()
-for i in json_page:
-    newNewsObject = news()
-    newNewsObject.assign_attributes(i)
-    news_list.append(newNewsObject)
-# for i in news_list:
-#     print(i)
+# david's trash
+# #http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-22&sortBy=publishedAt&apiKey=58e9f79801054ad698f961de67f7c258
+# page = get("http://newsapi.org/v2/top-headlines?country=us&apiKey=58e9f79801054ad698f961de67f7c258") 
+# page.encoding = page.apparent_encoding
+# json_page = dict(loads(page.text))["articles"]
+# news_list = list()
+# for i in json_page:
+#     newNewsObject = news()
+#     newNewsObject.assign_attributes(i)
+#     news_list.append(newNewsObject)
+# # for i in news_list:
+# #     print(i)
 
 # start the api app.
 app = Flask(__name__)
 app.static_folder = "static"
 
+# ian's trash
+def getRequests(arr):
+    nested_arr = []
+    for i in arr:
+        print(i)
+        url = "http://newsapi.org/v2/top-headlines?q=%s&from=2020-09-22&sortBy=publishedAt&apiKey=58e9f79801054ad698f961de67f7c258" % i
+        print(url)
+        page = get(url) 
+        page.encoding = page.apparent_encoding
+        json_page = dict(loads(page.text))["articles"]
+        news_list = list()
+        for i in json_page:
+            newNewsObject = news()
+            newNewsObject.assign_attributes(i)
+            news_list.append(newNewsObject)
+        nested_arr.append(news_list)
+    # for i in news_list:
+    #     print(i)
+    return nested_arr
+
+news_list = []
+topics_copy = []
+
 @app.route("/", methods=["GET", "POST", "DELETE"])  # main card view page
 def run():
-    return render_template("index.html",news_list=news_list) 
+    if request.method == "POST":
+        if "AddFilters" in request.form:
+            first = request.form["firstTopic"]
+            second = request.form["secondTopic"]
+            third = request.form["thirdTopic"]
+            fourth = request.form["fourthTopic"]
+            fifth = request.form["fifthTopic"]
+            topics = [first, second, third, fourth, fifth]
+
+            content = getRequests(topics)
+            for i in topics:
+                topics_copy.append(i)
+            
+            for i in content:
+                news_list.append(i)
+            
+        else:
+            pass
+    elif request.method == "DELETE":
+        if "RemoveCard" in request.form:
+            pass # hacer algo para quitar carta de news_content
+    
+    return render_template(
+        "index.html",
+        news_list=news_list,
+        topics_list=topics_copy
+        ) 
     
 if __name__ == "__main__":
     
     app.run(debug=True)
-
-
-
-# ian's trash
-# def getRequests(some array []):
-#    news_request = "https://idk_which_news_website.com"
-#    try:
-#        news_content = requests.get(news_request).text
-#    except:
-#        print(f"unable to get {news_request}")
-#        sys.exit(1)
-#    news_cards = json.loads(news_content)
-#    return news_cards
-
-
-# if request.method == "POST":
-#     if "AddFilters" in request.form:
-#         topics = request.form["exampleInputEmail1"]
-#         # content = getRequests(topics)
-#         # news_content.copy(content) # metodo copiar content a news_content no me recuerdo como era.
-        
-#         for i in news_content:
-#             print(i)
-#     else:
-#         pass
-# elif request.method == "DELETE":
-#     if "RemoveCard" in request.form:
-#         pass # hacer algo para quitar carta de news_content
-# return render_template(
-#     "index.html",
-#     news_array = news_content
-# )
