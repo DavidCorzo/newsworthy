1cb7eecab1b5c8cb6941646391ef9bf2dae173dc
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-10-22 14:54:34 -0600

    format

--- newsworthy.py
+++ newsworthy.py
@@ -6,7 +6,7 @@ import jinja2
 
 # david's trash
 # #http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-22&sortBy=publishedAt&apiKey=58e9f79801054ad698f961de67f7c258
-# page = get("http://newsapi.org/v2/top-headlines?country=us&apiKey=58e9f79801054ad698f961de67f7c258") 
+# page = get("http://newsapi.org/v2/top-headlines?country=us&apiKey=58e9f79801054ad698f961de67f7c258")
 # page.encoding = page.apparent_encoding
 # json_page = dict(loads(page.text))["articles"]
 # news_list = list()
@@ -22,13 +22,15 @@ app = Flask(__name__)
 app.static_folder = "static"
 
 # ian's trash
+
+
 def getRequests(arr):
     nested_arr = []
     for i in arr:
         print(i)
         url = "http://newsapi.org/v2/top-headlines?q=%s&from=2020-09-22&sortBy=publishedAt&apiKey=58e9f79801054ad698f961de67f7c258" % i
         print(url)
-        page = get(url) 
+        page = get(url)
         page.encoding = page.apparent_encoding
         json_page = dict(loads(page.text))["articles"]
         news_list = list()
@@ -41,8 +43,10 @@ def getRequests(arr):
     #     print(i)
     return nested_arr
 
+
 news_list = []
 topics_copy = []
+
 
 @app.route("/", methods=["GET", "POST", "DELETE"])  # main card view page
 def run():
@@ -58,22 +62,23 @@ def run():
             content = getRequests(topics)
             for i in topics:
                 topics_copy.append(i)
-            
+
             for i in content:
                 news_list.append(i)
-            
+
         else:
             pass
     elif request.method == "DELETE":
         if "RemoveCard" in request.form:
-            pass # hacer algo para quitar carta de news_content
-    
+            pass  # hacer algo para quitar carta de news_content
+
     return render_template(
         "index.html",
         news_list=news_list,
         topics_list=topics_copy
-        ) 
-    
+    )
+
+
 if __name__ == "__main__":
-    
+
     app.run(debug=True)
