366d51da57bab3f2142f9192cd27d7f2f8e06589
ianj090 <ianjenatz@ufm.edu>
2020-10-22 23:23:29 -0600

    fixed html

--- app.py
+++ app.py
@@ -1,52 +1,49 @@
-from flask import Flask, render_template, request
+import os
+from news import News
 from requests import get
 from json import loads
-from news import news
-import jinja2
+from flask import Flask, render_template, request
 
-# david's trash
-# #http://newsapi.org/v2/everything?q=bitcoin&from=2020-09-22&sortBy=publishedAt&apiKey=58e9f79801054ad698f961de67f7c258
-# page = get("http://newsapi.org/v2/top-headlines?country=us&apiKey=58e9f79801054ad698f961de67f7c258")
-# page.encoding = page.apparent_encoding
-# json_page = dict(loads(page.text))["articles"]
-# news_list = list()
-# for i in json_page:
-#     newNewsObject = news()
-#     newNewsObject.assign_attributes(i)
-#     news_list.append(newNewsObject)
-# # for i in news_list:
-# #     print(i)
-
-# start the api app.
 app = Flask(__name__)
 app.static_folder = "static"
 
-# ian's trash
-
-
-def getRequests(arr):
-    nested_arr = []
-    for i in arr:
-        print(i)
-        url = "http://newsapi.org/v2/top-headlines?q=%s&from=2020-09-24&sortBy=publishedAt&apiKey=537a36338c1e4d119f54dfec8f08ba9b" % i
-        print(url)
-        page = get(url)
-        page.encoding = page.apparent_encoding
-        json_page = dict(loads(page.text))["articles"]
-        news_list = list()
-        for i in json_page:
-            newNewsObject = news()
-            newNewsObject.assign_attributes(i)
-            news_list.append(newNewsObject)
-        nested_arr.append(news_list)
-    return nested_arr
-
+environment = os.getenv("ENVIRONMENT", "development")
 
 news_list = []
 topics_copy = []
 
 
-@app.route("/", methods=["GET", "POST", "DELETE"])  # main card view page
+def getRequests(arr):
+    nested_arr = []
+
+    for i in arr:
+        print(i)
+        url = "http://newsapi.org/v2/top-headlines?q=%s&from=2020-09-24&sortBy=publishedAt&apiKey=537a36338c1e4d119f54dfec8f08ba9b" % i
+
+        print(url)
+
+        page = get(url)
+        page.encoding = page.apparent_encoding
+        json_page = dict(loads(page.text))["articles"]
+        news_list = list()
+
+        for i in json_page:
+            newNewsObject = News()
+            newNewsObject.assign_attributes(i)
+            news_list.append(newNewsObject)
+        nested_arr.append(news_list)
+
+    # for i in news_list:
+    #     print(i)
+    return nested_arr
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/news", methods=["GET", "POST", "DELETE"])  # main card view page
 def run():
     if request.method == "POST":
         if "AddFilters" in request.form:
@@ -60,7 +57,6 @@ def run():
             content = getRequests(topics)
             for i in topics:
                 topics_copy.append(i)
-<<<<<<< HEAD
             for i in content:
                 news_list.append(i)
         
@@ -77,28 +73,18 @@ def run():
             pass
         
     
-=======
-
-            for i in content:
-                news_list.append(i)
-
-        else:
-            pass
-    elif request.method == "DELETE":
-        if "RemoveCard" in request.form:
-            pass  # hacer algo para quitar carta de news_content
-
->>>>>>> 1cb7eecab1b5c8cb6941646391ef9bf2dae173dc
     return render_template(
-        "index.html",
+        "news.html",
         news_list=news_list,
         topics_list=topics_copy
-    )
+        ) 
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-=======
 
->>>>>>> 1cb7eecab1b5c8cb6941646391ef9bf2dae173dc
-    app.run(debug=True)
+    debug = False
+
+    if environment == "development" or environment == "local":
+        debug = True
+
+    app.run(host="127.0.0.1", debug=debug)
--- news.py
+++ news.py
@@ -2,36 +2,39 @@ from requests import get
 from bs4 import BeautifulSoup
 from json import loads, dump
 
-class news:
+
+class News:
     """This class is dedicated to storing the values of selected news from the API link response in dict format. """
+
     def __init__(self):
-        self.author                     :str        = None
-        self.title                      :str        = None
-        self.description                :str        = None
-        self.url                        :str        = None
-        self.url_to_image               :str        = None
-        self.date_time_of_publishing    :str        = None
-        self.id                         :str        = None
-        self.name                       :str        = None
-    
+        self.author: str = None
+        self.title: str = None
+        self.description: str = None
+        self.url: str = None
+        self.url_to_image: str = None
+        self.date_time_of_publishing: str = None
+        self.id: str = None
+        self.name: str = None
+
     # setters and getters.
     def get_author(self): return self.author
     def set_author(self, author): self.author = author
-    
+
     def get_title(self): return self.title
     def set_title(self, title): self.title = title
-    
+
     def get_description(self): return self.description
     def set_description(self, description): self.description = description
-    
+
     def get_url(self): return self.url
     def set_url(self, url): self.url = url
-    
+
     def get_url_to_image(self): return self.url_to_image
     def set_url_to_image(self, url_to_image): self.url_to_image = url_to_image
-    
+
     def get_date_time_of_publishing(self): return self.date_time_of_publishing
-    def set_date_time_of_publishing(self, date_time_of_publishing): self.date_time_of_publishing = date_time_of_publishing
+    def set_date_time_of_publishing(
+        self, date_time_of_publishing): self.date_time_of_publishing = date_time_of_publishing
 
     def get_id(self, id): return self.id
     def set_id(self, id): self.id = id
@@ -39,44 +42,39 @@ class news:
     def get_name(self, name): return self.name
     def set_name(self, name): self.name = name
 
-    def assign_attributes(self,i:dict):
+    def assign_attributes(self, i: dict):
         try:
             self.set_id(i["source"]["id"])
-        except: 
+        except:
             pass
         try:
             self.set_name(i["source"]["name"])
-        except: 
+        except:
             pass
         try:
             self.set_author(i["author"])
-        except: 
+        except:
             pass
         try:
             self.set_title(i["title"])
-        except: 
+        except:
             pass
         try:
             self.set_description(i["description"])
-        except: 
+        except:
             pass
         try:
             self.set_url(i["url"])
-        except: 
+        except:
             pass
         try:
             self.set_url_to_image(i["urlToImage"])
-        except: 
+        except:
             pass
         try:
             self.set_date_time_of_publishing(i["publishedAt"])
-        except: 
+        except:
             pass
 
     def __str__(self):
         return f"author: {self.author}, title: {self.title} \n" + "-"*60
-
-
-
-    
-
